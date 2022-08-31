ca6c3f0928e66ded1c08d4e584195a71dea16f6c
Eddie Pace <eddie.pace@cydar.co.uk>
2022-08-31 17:13:26 +0100

    add option for description and usage to script

--- add-term-to-glossary.sh
+++ add-term-to-glossary.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# Script to quickly add a new template entry to the glossary and a link to the new entry in the index at the top.
+# alias as somethign like "gloss"
+# usage: gloss "New term" "description of term"
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 FILE_PATH=$SCRIPT_DIR/README.md;
 
 # Write glossary title
 echo "## $1
-(***acronym definition?***)" >> $FILE_PATH;
+(***acronym definition?***) $2" >> $FILE_PATH;
 
 # Write index linked to glossary entry
 END_OF_INDEX=$(grep -n "<!--END-OF-INDEX-->" $FILE_PATH);
